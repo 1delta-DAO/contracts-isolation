25acd0cc876a81981d8667ea7a18d1b2eb8f1201
Achthar <69119535+Achthar@users.noreply.github.com>
2023-07-27 10:17:15 +0200

    add quoter live test

--- scripts/zk-vix/quoter.ts
+++ scripts/zk-vix/quoter.ts
@@ -1,15 +1,6 @@
 import '@nomiclabs/hardhat-ethers'
 import hre from 'hardhat'
-import { FeeAmount } from '../../test/uniswap-v3/core/shared/utilities';
-import { encodePath } from '../../test/uniswap-v3/periphery/shared/path';
 import { AggregationQuoter__factory } from '../../types';
-
-
-const usdtAddress = "0x1E4a5963aBFD975d8c9021ce480b42188849D41d"
-const usdcAddress = "0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035"
-const daiAddress = '0xC5015b9d9161Dca7e18e32f6f25C4aD850731Fd4'
-const wethAddress = '0x4F9A0e7FD2Bf6067db6994CF12E4495Df938E6e9'
-const wbtcAddress = '0xEA034fb02eB1808C2cc3adbC15f447B93CbE08e1'
 
 async function main() {
 
@@ -25,30 +16,15 @@ async function main() {
     const DOV_FF_FACTORY_ADDRESS = '0xdE474Db1Fa59898BC91314328D29507AcD0D593c';
     const DOV_POOL_INIT_CODE_HASH = '0xd3e7f58b9af034cfa7a0597e539bae7c6b393817a47a6fc1e1503cd6eaffe22a';
 
-    const quoter = await new AggregationQuoter__factory(operator).attach(
-        '0xf9b36A3FAFa4AD006b360b3CDdb7DAA72F299180'
-        )
-    
-    // .deploy(
-    //     ALG_FF_FACTORY_ADDRESS,
-    //     DOV_FF_FACTORY_ADDRESS,
-    //     ALG_POOL_CODE_HASH,
-    //     DOV_POOL_INIT_CODE_HASH
-    // )
+    const quoter = await new AggregationQuoter__factory(operator).deploy(
+        ALG_FF_FACTORY_ADDRESS,
+        DOV_FF_FACTORY_ADDRESS,
+        ALG_POOL_CODE_HASH,
+        DOV_POOL_INIT_CODE_HASH
+    )
 
     console.log('Addresses')
     console.log('quoter:', quoter.address)
-
-    const path = encodePath(
-        [usdcAddress, wbtcAddress, wethAddress],
-        [FeeAmount.ALGEBRA, FeeAmount.ALGEBRA]
-
-    )
-
-    const swapAmount = 10000000
-    const quote = await quoter.callStatic.quoteExactInput(path, swapAmount)
-    console.log("Quote received", quote.toString())
-
 }
 
 main()
@@ -59,4 +35,5 @@ main()
     });
 
 
-    // quoter: 0xf9b36A3FAFa4AD006b360b3CDdb7DAA72F299180
+// quoter: 0xf9b36A3FAFa4AD006b360b3CDdb7DAA72F299180
+    
